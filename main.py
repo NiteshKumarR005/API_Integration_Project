88f7615f7c139ce224cc2819342b32ed59d62910
NiteshKumarR005 <niteshkumar2k05@gmail.co>
2026-06-29 18:53:38 +0530

    Implemented error handling, continuous searching, and testing.

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import requests
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-response = requests.get(url)
+try:
+    
 
-coin = input("Enter cryptocurrency name: ").lower().strip()
-params = {"crypto_id": coin, "currency": "usd"}
+    while True:
+        coin = input("Enter cryptocurrency name (or 'exit' to quit): ").lower().strip()
+        if coin == "exit":
+            print("Task ended.")
+            break
+        
+        url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies=usd"
+        response = requests.get(url)
+        
+        params = {"crypto_id": coin, "currency": "usd"}
 
-response = requests.get(url, params=params)
+        response = requests.get(url, params=params)
 
-data = response.json()
-price = data[coin]["usd"]
+        data = response.json()
+        if coin in data:
+            price = data[coin]["usd"]
 
-print("----- Result -----")
-print(f" Coin: {coin}\n Price: {price}")
-print("------------------")
+            print("----- Result -----")
+            print(f" Coin: {coin}\n Price: {price}")
+            print("------------------")
+        else:
+            print("Coin not found.")
+    
+except requests.exception.RequestException:
+    print("Network error")
