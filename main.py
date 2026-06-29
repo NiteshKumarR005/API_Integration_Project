adfd453b17e12816dd0621f6bfa68cf089a0d7ea
NiteshKumarR005 <niteshkumar2k05@gmail.co>
2026-06-29 18:19:47 +0530

    Implemented search functionality and data display features

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import requests
 url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 response = requests.get(url)
 
-coin = "bitcoin"
-params = {"hello": coin, "currency": "usd"}
+coin = input("Enter cryptocurrency name: ").lower().strip()
+params = {"crypto_id": coin, "currency": "usd"}
 
 response = requests.get(url, params=params)
 
 data = response.json()
-print(data)
+price = data[coin]["usd"]
+
+print("----- Result -----")
+print(f" Coin: {coin}\n Price: {price}")
+print("------------------")
